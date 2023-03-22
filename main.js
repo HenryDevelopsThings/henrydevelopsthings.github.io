c3ef8d7300b82cb70088ac0d07a46aa6712805c4
Leppy <henry.dampier07@gmail.com>
2023-03-22 17:22:03 +0000

    Added files

---
+++ main.js
@@ -0,0 +1,18 @@
+var typedHome = new Typed(".multiple-text-job", {
+    strings: ["Game Developer", "3D Modeller", "Full-stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typedProject = new Typed(".project-content-text", {
+    strings: ["Coming Soon", "Imminent", "In The Pipeline", "In Progress", "On The Horizon", "Anticipated", "On its way", "Just around the corner", "Approaching", "Coming down the pike", "Almost here", "In the works", "Upcoming", "In progress", "In development", "In the making", "In the offing", "In the near future", "Just ahead", "Near at hand", "In the pipeline", "In the queue", "In the hopper", "En route", "In transit", "On the horizon", "Anticipated", "On the verge", "On the brink", "Imminent", "Impending", "Coming down the pipeline", "Coming down the chute", "Coming down the track", "In the coming weeks/months", "In the near-term", "Up and coming", "Up next", "In the near vicinity", "In the immediate future", "Just over the horizon", "Close on the heels", "In the foreseeable future", "At hand", "Just in time", "On the way soon", "Coming your way", "Just moments away", "On the cusp", "Not far off", "Nearing completion", "Soon to arrive", "Just up ahead", "Around the bend", "In the wings", "On the doorstep"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    shuffle: true,
+    fadeOut: true,
+    fadeOutDelay: 500,
+    loop: true
+})
